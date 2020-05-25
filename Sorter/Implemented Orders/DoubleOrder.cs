eb216b1612e724ee85c3073b1d061c40df62a97d
Kalon Shannon-Innes <18529687@student.curtin.edu.au>
2020-05-25 21:55:35 +0800

    Add DoubleOrder.cs

---
+++ Sorter/Implemented Orders/DoubleOrder.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace Sorter.Implemented_Orders
+{
+    public class DoubleOrder: Order
+    {
+        /// <summary>
+        ///     Return true if the first double parameter is less than the second double parameter
+        /// </summary>
+        public override bool LessThan(object x, object y)
+        {
+            return (double) x < (double) y;
+        }
+
+        /// <summary>
+        ///     Return true if the two double values are equal to a precision of 9 digits after the decimal point
+        /// </summary>
+        public override bool Equal(object x, object y)
+        {
+            const double nineDigitPrecision = 0.000000001;
+            return Math.Abs((double) x - (double) y) < nineDigitPrecision;
+        }
+    }
+}
