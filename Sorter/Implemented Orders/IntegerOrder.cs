2fc3ff244a31f1ffc8db0d98bd35c2c3310377df
Kalon Shannon-Innes <18529687@student.curtin.edu.au>
2020-05-25 21:55:53 +0800

    Add comments to IntegerOrder.cs

--- Sorter/Implemented Orders/IntegerOrder.cs
+++ Sorter/Implemented Orders/IntegerOrder.cs
@@ -2,11 +2,20 @@
 {
     public class IntegerOrder : Order
     {
+        /// <summary>
+        ///     Return true if the first integer value is less than the second integer value
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
         public override bool LessThan(object x, object y)
         {
             return (int) x < (int) y;
         }
 
+        /// <summary>
+        ///     Return true if the two integer values are equivalent
+        /// </summary>
         public override bool Equal(object x, object y)
         {
             return (int) x == (int) y;
