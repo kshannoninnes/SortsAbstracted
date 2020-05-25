fe04b8d921671bc6ff0b3504fea41b5a11d81fb0
Kalon Shannon-Innes <18529687@student.curtin.edu.au>
2020-05-25 18:16:25 +0800

    Fix HeapSort

--- Sorter/Implemented Sorts/HeapSort.cs
+++ Sorter/Implemented Sorts/HeapSort.cs
@@ -9,25 +9,33 @@ namespace Sorter.Implemented_Sorts
 
         protected override async Task<int> Split(IList list, int low, int high)
         {
+            if (high == list.Count - 1) // If this is the first time splitting
+            {
+                // Build Max Heap
+                for (var ii = high / 2 - 1; ii >= 0; ii--)
+                    await MaxHeapify(list, high, ii);
+            }
+            
             await Swap(list, low, high);
-            await MinHeapify(list, low, high - 1);
+            await MaxHeapify(list, high - 1, low);
             return high;
         }
 
-        private async Task MinHeapify(IList list, int ii, int heapSize)
+        private async Task MaxHeapify(IList list, int n, int ii)
         {
-            await Task.Delay(1);
-            var smallest = ii;
+            await Task.Delay(1); // Animation purposes
+            
+            var largest = ii;
             var leftChild = ii * 2 + 1;
             var rightChild = ii * 2 + 2;
 
-            if (leftChild <= heapSize && Order.LessThan(list[leftChild], list[smallest])) smallest = leftChild;
-            if (rightChild <= heapSize && Order.LessThan(list[rightChild], list[smallest])) smallest = rightChild;
+            if (leftChild <= n && Order.GreaterThan(list[leftChild], list[largest])) largest = leftChild;
+            if (rightChild <= n && Order.GreaterThan(list[rightChild], list[largest])) largest = rightChild;
 
-            if (smallest != ii)
+            if (largest != ii)
             {
-                await Swap(list, ii, smallest);
-                await MinHeapify(list, smallest, heapSize);
+                await Swap(list, ii, largest);
+                await MaxHeapify(list, n, largest);
             }
         }
 
