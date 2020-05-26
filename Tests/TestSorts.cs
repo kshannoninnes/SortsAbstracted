e0ca307b2ac35a0cd9499d3ffe7f224c235f7ae1
Kalon Shannon-Innes <18529687@student.curtin.edu.au>
2020-05-27 01:59:06 +0800

    Refactor tests and expand test set

--- Tests/TestSorts.cs
+++ Tests/TestSorts.cs
@@ -11,52 +11,67 @@ namespace Tests
     public abstract class TestSorts
     {
         protected Sorter.AbstractSort TestAbstractSort;
+        private object[] _expected;
+
+        [OneTimeSetUp]
+        public Task OneTimeSetup()
+        {
+            _expected = new object[50];
+
+            for (var ii = 0; ii < _expected.Length; ii++)
+            {
+                _expected[ii] = ii + 1;
+            }
+            
+            return Task.CompletedTask;
+        }
 
         [Test]
         public async Task Sort_ReverseSortedArray_IsSorted()
         {
-            var input = new object[] {5, 4, 3, 2, 1};
-            var expected = new object[] {1, 2, 3, 4, 5};
+            var input = new object[_expected.Length];
+            Array.Copy(_expected, input, _expected.Length);
+            Array.Reverse(input);
             
             var timer = new Stopwatch();
             timer.Start();
-            await TestAbstractSort.Sort(input, 0, 4);
+            await TestAbstractSort.Sort(input, 0, input.Length - 1);
             timer.Stop();
             
             Console.WriteLine($"Test running time: {timer.Elapsed}");
-            Assert.AreEqual(expected, input);
+            Assert.AreEqual(_expected, input);
         }
 
         [Test]
         public async Task Sort_PreSortedArray_IsSorted()
         {
-            var input = new object[] {1, 2, 3, 4, 5};
-            var expected = new object[] {1, 2, 3, 4, 5};
+            var input = new object[_expected.Length];
+            Array.Copy(_expected, input, _expected.Length);
             
             var timer = new Stopwatch();
             timer.Start();
-            await TestAbstractSort.Sort(input, 0, 4);
+            await TestAbstractSort.Sort(input, 0, input.Length - 1);
             timer.Stop();
             
             Console.WriteLine($"Test running time: {timer.Elapsed}");
-            Assert.AreEqual(expected, input);
+            Assert.AreEqual(_expected, input);
         }
 
         [Test]
         public async Task Sort_RandomlySortedArray_IsSorted()
         {
             var rand = new Random();
-            var input = new object[] {1, 2, 3, 4, 5};
+            var input = new object[_expected.Length];
+            Array.Copy(_expected, input, _expected.Length);
             input = input.OrderBy(x => rand.Next()).ToArray();
-            var expected = new object[] {1, 2, 3, 4, 5};
             
             var timer = new Stopwatch();
             timer.Start();
-            await TestAbstractSort.Sort(input, 0, 4);
+            await TestAbstractSort.Sort(input, 0, input.Length - 1);
             timer.Stop();
             
             Console.WriteLine($"Test running time: {timer.Elapsed}");
-            Assert.AreEqual(expected, input);
+            Assert.AreEqual(_expected, input);
         }
     }
 }
